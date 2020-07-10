d74454f0852c1405ab6a442c541d8e0752ce626f
Lucas Santana <lucas_377@hotmail.com>
2020-07-10 12:10:01 -0300

    Tabuada completa.

--- aula1/tabuada/script.js
+++ aula1/tabuada/script.js
@@ -1,4 +1,11 @@
 function tab(){
-    var nb = document.getElementById('txtnb')
-    alert(nb)
+    var nb1 = document.getElementById('txtnb')
+    var n1 = Number(nb1.value)
+    
+    res = document.getElementById('resultado')
+    res.innerHTML = ``
+
+    for(var c = 0; c < 11; c++){
+        res.innerHTML += `${n1} x ${c} = <strong>${n1 * c}</strong><br>`
+    }
 }
